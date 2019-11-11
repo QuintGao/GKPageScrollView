31dcec035a5062dde056796fbba7cde0051bd7d9
QuintGao <1094887059@qq.com>
2019-11-11 17:08:26 +0800

    修复GKPageScrollView与返回手势的冲突

--- GKPageScrollView/objc/GKPageListContainerView.m
+++ GKPageScrollView/objc/GKPageListContainerView.m
@@ -31,12 +31,36 @@
             }
         }
     }
+    
+    if ([self panBack:gestureRecognizer]) return NO;
+    
     return YES;
 }
 
 - (BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldRecognizeSimultaneouslyWithGestureRecognizer:(UIGestureRecognizer *)otherGestureRecognizer {
     if ([self.gestureDelegate respondsToSelector:@selector(pageListContainerCollectionView:gestureRecognizer:shouldRecognizeSimultaneouslyWithGestureRecognizer:)]) {
         return [self.gestureDelegate pageListContainerCollectionView:self gestureRecognizer:gestureRecognizer shouldRecognizeSimultaneouslyWithGestureRecognizer:otherGestureRecognizer];
+    }
+    
+    if ([self panBack:gestureRecognizer]) return YES;
+    
+    return NO;
+}
+
+- (BOOL)panBack:(UIGestureRecognizer *)gestureRecognizer {
+    if (gestureRecognizer == self.panGestureRecognizer) {
+        CGPoint point = [self.panGestureRecognizer translationInView:self];
+        UIGestureRecognizerState state = gestureRecognizer.state;
+        
+        // 设置手势滑动的位置距屏幕左边的区域
+        CGFloat locationDistance = [UIScreen mainScreen].bounds.size.width;
+        
+        if (state == UIGestureRecognizerStateBegan || state == UIGestureRecognizerStatePossible) {
+            CGPoint location = [gestureRecognizer locationInView:self];
+            if (point.x > 0 && location.x < locationDistance && self.contentOffset.x <= 0) {
+                return YES;
+            }
+        }
     }
     return NO;
 }
